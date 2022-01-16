1731da7386230969b9c6dba2f5baa06566ac53db
jinn2u <bsybear623@gmail.com>
2022-01-16 20:11:19 +0900

    feat: 버튼 클릭시 랜덤으로 배경색 바뀌게 하기

--- jinn2u/Color/src/components/App.js
+++ jinn2u/Color/src/components/App.js
@@ -3,7 +3,21 @@ import { isCalledWithNew } from '../utils';
 import Button from './Button';
 
 function App({ $target }) {
+  this.$target = null;
   isCalledWithNew(new.target);
-  (() => new Button({ $target, text: 'click me!' }))();
+  this.$target = $target;
+
+  (() =>
+    new Button({
+      $target,
+      text: 'click me!',
+      onBtnClick: () => {
+        const createRandomRgb = () => Math.round(Math.random() * 255);
+        const r = createRandomRgb();
+        const g = createRandomRgb();
+        const b = createRandomRgb();
+        this.$target.style.backgroundColor = `rgba(${r},${g},${b})`;
+      },
+    }))();
 }
 export default App;
--- jinn2u/Color/src/components/Button.js
+++ jinn2u/Color/src/components/Button.js
@@ -1,22 +1,34 @@
 import { isCalledWithNew } from '../utils';
 
-function Button({ $target, text }) {
+function Button({ $target, text, onBtnClick }) {
+  this.$target = null;
+  this.text = null;
+  this.onBtnClick = null;
+
   isCalledWithNew(new.target);
+  this.$target = $target;
+  this.text = text;
+  this.onBtnClick = onBtnClick;
+
   const $button = document.createElement('button');
   $button.setAttribute('type', 'button');
   $target.appendChild($button);
 
   this.render = () => {
-    this._verifyText();
-    $button.innerText = text;
+    this._verifyParams();
+    $button.innerText = this.text;
   };
-  this._verifyText = () => {
-    if (typeof text !== 'string') {
+
+  this._verifyParams = () => {
+    if (typeof this.text !== 'string') {
       return console.warn(`버튼안의 ${text}는 문자여야 합니다.`);
+    }
+    if (typeof this.onBtnClick !== 'function') {
+      return console.warn('onBtnClick은 함수여야 합니다.');
     }
     return true;
   };
-
+  $button.addEventListener('click', this.onBtnClick);
   this.render();
 }
 export default Button;
