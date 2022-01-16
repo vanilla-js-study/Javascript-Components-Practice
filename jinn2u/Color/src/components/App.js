b766072b49ccf6f3bbb78d79783eada9e2a2b60c
jinn2u <bsybear623@gmail.com>
2022-01-16 20:26:58 +0900

    feat: state가 변경되었을 때 render 실행하기

--- jinn2u/Color/src/components/App.js
+++ jinn2u/Color/src/components/App.js
@@ -4,6 +4,14 @@ import Button from './Button';
 
 function App({ $target }) {
   this.$target = null;
+  this.state = {
+    colors: { r: 0, g: 0, b: 0 },
+  };
+  this._setState = (nextState) => {
+    this.state = nextState;
+    this._render();
+  };
+
   isCalledWithNew(new.target);
   this.$target = $target;
 
@@ -16,8 +24,13 @@ function App({ $target }) {
         const r = createRandomRgb();
         const g = createRandomRgb();
         const b = createRandomRgb();
-        this.$target.style.backgroundColor = `rgba(${r},${g},${b})`;
+        this._setState({ ...this.state, colors: { r, g, b } });
       },
     }))();
+
+  this._render = () => {
+    const { r, g, b } = this.state.colors;
+    this.$target.style.backgroundColor = `rgba(${r},${g},${b})`;
+  };
 }
 export default App;
