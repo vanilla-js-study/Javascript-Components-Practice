db7b6dcd4df2f85001389eee76941934a997c35e
Jeong Ho <gkood2@gamil.com>
2022-02-02 21:43:07 +0900

    안내 문구, 현재 배경색상 정보 문구 추가

--- palsa131/HexColorsGradient/src/App.js
+++ palsa131/HexColorsGradient/src/App.js
@@ -1,5 +1,25 @@
 import Button from "./Button.js"
 export default function App({targetEl}){
+	
+	this.state ={
+		linearGradient: ['#ffffff','#ffffff'],
+	};
+
+	this.setLinearGradient = (nextState)=>{
+		this.state.linearGradient = nextState;
+		this.render();
+	}
+
+	const guideTextEl = document.createElement('h1');
+	const backgroundInfoTextEl = document.createElement('h2');
+
+	this.render = ()=>{
+		guideTextEl.textContent = 'CLICK THE BUTTON BELLOW TO GENERATE A RANDOM GRADIENT HEX COLOR COMBINATION'
+		backgroundInfoTextEl.textContent = `background: linear-gradient(to right, ${this.state.linearGradient[0]}, ${this.state.linearGradient[1]});`
+		
+		targetEl.appendChild(guideTextEl);
+		targetEl.appendChild(backgroundInfoTextEl);
+	}
 
 	const button = new Button({
 		targetEl,
@@ -9,7 +29,10 @@ export default function App({targetEl}){
 		onClick: ()=>{
 			const linearGradient = Array(2).fill(0).map(()=> Array(3).fill(0).map(()=>Math.floor(Math.random() * 256).toString(16)).join(''))
 			targetEl.style.background = `linear-gradient(to right,#${linearGradient[0]},#${linearGradient[1]})`;
+			this.setLinearGradient(linearGradient);
 		},
 	})
+
+	this.render();
 }
 
